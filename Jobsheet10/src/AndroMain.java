409da5174f2d2766427cea6378bff6dc55bb3c8a
RaniNabillaPutri <112648914+RaniNabillaPutri@users.noreply.github.com>
2022-11-09 10:02:40 +0700

    Add files via upload

---
+++ Jobsheet10/src/AndroMain.java
@@ -0,0 +1,12 @@
+//created by 21343034_Rani Nabilla Putri
+
+public class AndroMain {
+    public static void main(String[] args){
+        Android android = new Android();
+
+        android.nyala();
+        android.panggilan();
+        android.sms();
+        android.shutdown();
+    }
+}
---
+++ Jobsheet10/src/Mahasiswa.java
@@ -0,0 +1,6 @@
+//created by 21343034_Rani Nabilla Putri
+
+public class Mahasiswa {
+    String nama, jur, univ;
+    int nim;
+}
